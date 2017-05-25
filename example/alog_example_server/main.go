ad790c1d0f45daa3e383de85acc7a0274410187a
Gabe Hart <ghart@us.ibm.com>
2017-05-25 10:48:53 -0600

    master: Port alog_example_server from disco-data-science

---
+++ example/alog_example_server/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"github.ibm.com/watson-discovery/alog"
+	"net/http"
+	"strings"
+)
+
+var ch = alog.UseChannel("MAIN")
+
+func main() {
+
+	// Get a port from the command line
+	listenPort := flag.String(
+		"port",
+		"54321",
+		"port bound to this service")
+
+	// Set up logging from command line
+	logFlags := alog.GetFlags()
+	flag.Parse()
+	if err := alog.ConfigureFromFlags(logFlags); nil != err {
+		alog.Fatalf("MAIN", alog.FATAL, err.Error())
+	}
+	ch.Log(alog.INFO, "Hello World!")
+
+	// Bind dynamic log handler
+	http.HandleFunc("/logging", alog.DynamicHandler)
+
+	// Bind simple function that does some logging
+	http.HandleFunc("/demo", func(w http.ResponseWriter, r *http.Request) {
+		ch := alog.UseChannel("HNDLR")
+		defer ch.LogScope(alog.TRACE, "Handling /demo").Close()
+
+		ch.Log(alog.WARNING, "WATCH OUT!")
+		ch.Log(alog.INFO, "Standard stuff...")
+		if ch.IsEnabled(alog.DEBUG) {
+			ch.Log(alog.DEBUG, "Query Params:")
+			r.ParseForm()
+			for k, vals := range r.Form {
+				ch.Log(alog.DEBUG, "  * %s: %s", k, strings.Join(vals, ", "))
+			}
+		}
+		w.WriteHeader(http.StatusOK)
+	})
+
+	// Start serving requests
+	ch.Log(alog.FATAL, "%s", http.ListenAndServe(":"+*listenPort, nil))
+}
