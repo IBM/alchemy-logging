4943afe0a9ebbe2a23cb55745fb998e5c7d973df
Gabe Goodhart <ghart@us.ibm.com>
2019-08-09 17:03:55 -0600

    master: Expose standard formatters in the module

--- alog/__init__.py
+++ alog/__init__.py
@@ -20,3 +20,6 @@ from .alog import configure, use_channel, ScopedLog, FnLog, ScopedTimer
 # Exposed details
 from .alog import g_alog_level_to_name as _level_to_name
 from .alog import g_alog_level_to_name as _name_to_level
+from .alog import AlogFormatterBase
+from .alog import AlogPrettyFormatter
+from .alog import AlogJsonFormatter
