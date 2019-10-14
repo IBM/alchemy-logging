e9bd0f813a9959ff968a6f4b378cc28bbb59a1e2
Gabe Goodhart <ghart@us.ibm.com>
2019-10-14 10:31:37 -0600

    CI: Update args in setup.py

--- setup.py
+++ setup.py
@@ -12,15 +12,12 @@ setup(
   description="A wrapper around the logging package to provide Alchemy Log functionality",
   long_description=long_description,
   url="https://github.ibm.com/watson-nlu/alog-py",
-  author="Gabe Hart - IBM Watson",
+  author="Gabe Goodhart - IBM Watson",
   author_email="ghart@us.ibm.com",
-  license=None, # Not sure what should go here since it's IBM code
+  license='Copyright IBM 2019 -- All rights reserved.',
   classifiers=[
-    'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
     'Topic :: Software Development :: User Interfaces',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
   ],
