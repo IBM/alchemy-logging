5fb03e05384c53c78527a1d332068eb74aa40880
Gabe Goodhart <ghart@us.ibm.com>
2020-05-26 17:13:07 -0600

    PerThreadIndent: No need for .coveragerc

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -5,7 +5,6 @@ BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$BASE_DIR"
 
 python3 -m pytest \
-    --cov-config=.coveragerc \
     --cov=alog \
     --cov-report=term \
     --cov-report=html \
