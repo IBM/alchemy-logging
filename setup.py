84d998195ee88912516d95e9d69e970be588d2fb
Gabriel Goodhart <ghart@us.ibm.com>
2020-06-18 17:20:36 -0600

    Bump to 1.2.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(path.abspath(path.dirname(__file__)), "README.md")) as f:
 
 setup(
   name="alog",
-  version="1.2.0",
+  version="1.2.1",
   description="A wrapper around the logging package to provide Alchemy Log functionality",
   long_description=long_description,
   url="https://github.ibm.com/watson-nlu/alog-py",
