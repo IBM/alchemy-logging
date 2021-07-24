2a23af86eb662697b07e0fbacfb3b8fabd30b305
Gabe Goodhart <ghart@us.ibm.com>
2021-07-24 16:31:57 -0600

    PythonPackage: Add a script for publishing to pypi

---
+++ src/python/ci/publish.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Run from the base of the python directory
+cd $(dirname ${BASH_SOURCE[0]})/..
+
+# Clear out old publication files in case they're still around
+rm -rf build dist alchemy_logging.egg-info/
+
+# Build
+python setup.py sdist bdist_wheel
+
+# Publish to PyPi
+twine upload \
+    --username "__token__" \
+    --password "$PYPI_TOKEN" \
+    ${UPLOAD_REPO_ARG:-""} \
+    dist/*
+
+# Clean up
+rm -rf build dist alchemy_logging.egg-info/
