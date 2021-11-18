1878a0a00f82545a951c589f93c408cf5bff7a05
Gabe Goodhart <ghart@us.ibm.com>
2021-11-18 16:28:20 -0700

    PythonImportConfigure: Only add level and is_enabled at import time, not a full configure
    https://github.com/IBM/alchemy-logging/issues/99

--- src/python/tests/test_alog_clean_import.py
+++ src/python/tests/test_alog_clean_import.py
@@ -22,7 +22,12 @@
 # SOFTWARE.
 ################################################################################
 
-# Import the implementation details so that we can test them
+# Standard
+import logging
+import io
+import re
+
+# Local
 import alog
 
 def test_log_before_configure():
@@ -31,3 +36,41 @@ def test_log_before_configure():
     """
     ch = alog.use_channel("TEST")
     ch.debug2("No throw")
+
+
+def test_default_log_configuration_works():
+    '''We need to run this in a process that has a clean set of imports so that
+    we can evaluate the import behavior. Using importlib.reload in the main
+    process does not actually rerun the import-time code in alog.
+    '''
+    # Set up some standard logging with custom formatting
+    log_stream = io.StringIO()
+    stream_handler = logging.StreamHandler(stream=log_stream)
+    fmt = '%(asctime)s [%(levelname)s] %(message)s'
+    logging.basicConfig(
+        level=logging.INFO,
+        format=fmt,
+        handlers=[stream_handler],
+    )
+
+    # NOTE: This really sucks, but it seems to be impossible to get pytest to
+    #   not pre-configure the logging package, so we have to override the config
+    #   for the child logger explicitly. This is NOT necessary when running the
+    #   content of this test in a plain vanilla python session, but since pytest
+    #   does some configuration of its own before the test starts, the above
+    #   basicConfig call doesn't override that configuration.
+    logger = logging.getLogger('mychan')
+    logger.setLevel(logging.INFO)
+    logger.addHandler(stream_handler)
+    stream_handler.formatter = logging.Formatter(fmt)
+
+    # Make sure debug2 works
+    logger.debug2('Yep')
+    assert log_stream.getvalue() == ''
+
+    # Log to a standard enabled level and make sure the formatting is right
+    # 2021-11-18 15:16:07,468 [INFO] test123
+    logger.info('message')
+    log_lines = list(filter(lambda x: x, log_stream.getvalue().split('\n')))
+    assert len(log_lines) == 1
+    assert re.match(r'\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d,\d\d\d \[INFO\] message', log_lines[0])
