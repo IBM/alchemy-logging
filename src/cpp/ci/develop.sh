af7b2207df2434c4a64b744bb8c8f194b4f04adf
Gabe Goodhart <ghart@us.ibm.com>
2023-05-10 09:48:38 -0600

    FixCppBuild: Bupdate develop.sh to always target x86 if possible
    Eventually, it would be great to support arm builds as well, but for now
    we use an x86 target for a consistent build environment
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- src/cpp/ci/develop.sh
+++ src/cpp/ci/develop.sh
@@ -4,7 +4,12 @@
 cd $(dirname ${BASH_SOURCE[0]})/..
 
 # Build the base as the develop shell
-docker build . --target=base -t alog-cpp-develop
+build_cmd="docker build"
+if docker buildx version 2>&1 > /dev/null
+then
+    build_cmd="docker buildx build --platform=linux/amd64"
+fi
+$build_cmd . --target=base -t alog-cpp-develop
 
 # Run with source mounted
 docker run --rm -it -v $PWD:/src alog-cpp-develop
