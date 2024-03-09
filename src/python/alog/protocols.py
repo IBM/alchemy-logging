2c9206380f92298e7dc406041022622399901067
Mynhardt Burger <Mynhardt.Burger@ibm.com>
2024-03-09 16:46:35 -0500

    protocol updates
    Signed-off-by: Mynhardt Burger <Mynhardt.Burger@ibm.com>

--- src/python/alog/protocols.py
+++ src/python/alog/protocols.py
@@ -26,7 +26,19 @@ Alchemy Logger overrides on the logging.Logger instances.
 """
 
 import sys
-from typing import TYPE_CHECKING, ClassVar, List, Mapping, Optional, Protocol
+from logging import Filter, LogRecord
+from typing import (
+    TYPE_CHECKING,
+    Callable,
+    ClassVar,
+    List,
+    Mapping,
+    Optional,
+    Protocol,
+    Set,
+    Tuple,
+    Union,
+)
 
 from .alog import _Level
 
@@ -34,7 +46,6 @@ if TYPE_CHECKING:
     from logging import (
         Handler,
         Logger,
-        LogRecord,
         Manager,
         RootLogger,
         _ArgsType,
@@ -45,7 +56,79 @@ if TYPE_CHECKING:
     from typing_extensions import Self
 
 
-class LoggerProtocol(Protocol):
+class _SupportsFilter(Protocol):
+    def filter(self, __record: LogRecord) -> bool: ...
+
+
+_FilterType = Union[Filter, Callable[[LogRecord], bool], _SupportsFilter]
+
+
+class FilterProtocol(Protocol):
+    """
+    A base class for loggers and handlers which allows them to share
+    common code.
+    """
+
+    filters: List[_FilterType]
+
+    def __init__(self) -> None:
+        """
+        Initialize the list of filters to be an empty list.
+        """
+
+    def addFilter(self, filter: _FilterType) -> None:  # pylint: disable=invalid-name
+        """
+        Add the specified filter to this handler.
+        """
+
+    def removeFilter(self, filter: _FilterType) -> None:  # pylint: disable=invalid-name
+        """
+        Remove the specified filter from this handler.
+        """
+
+    def filter(self, record: LogRecord) -> bool:  # type: ignore
+        """
+        Determine if a record is loggable by consulting all the filters.
+
+        The default is to allow the record to be logged; any filter can veto
+        this and the record is then dropped. Returns a zero value if a record
+        is to be dropped, else non-zero.
+
+        .. versionchanged:: 3.2
+
+           Allow filters to be just callables.
+        """
+
+    if sys.version_info >= (3, 12):
+
+        def filter(self, record: LogRecord) -> Union[bool, LogRecord]:  # type: ignore pylint: disable=function-redefined
+            """
+            Determine if a record is loggable by consulting all the filters.
+
+            The default is to allow the record to be logged; any filter can veto
+            this by returning a false value.
+            If a filter attached to a handler returns a log record instance,
+            then that instance is used in place of the original log record in
+            any further processing of the event by that handler.
+            If a filter returns any other true value, the original log record
+            is used in any further processing of the event by that handler.
+
+            If none of the filters return false values, this method returns
+            a log record.
+            If any of the filters return a false value, this method returns
+            a false value.
+
+            .. versionchanged:: 3.2
+
+               Allow filters to be just callables.
+
+            .. versionchanged:: 3.12
+               Allow filters to return a LogRecord instead of
+               modifying it in place.
+            """
+
+
+class LoggerProtocol(FilterProtocol):
     """
     A Protocol reflecting the public methods, type hints and docstrings of the stdlib
     `logging.Logger`
@@ -65,17 +148,17 @@ class LoggerProtocol(Protocol):
         Initialize the logger with a name and an optional level.
         """
 
-    def setLevel(self, level: "_Level") -> None:
+    def setLevel(self, level: "_Level") -> None:  # pylint: disable=invalid-name
         """
         Set the logging level of this logger.  level must be an int or a str.
         """
 
-    def isEnabledFor(self, level: int) -> bool:  # type: ignore
+    def isEnabledFor(self, level: int) -> bool:  # type: ignore pylint: disable=invalid-name
         """
         Is this logger enabled for level 'level'?
         """
 
-    def getEffectiveLevel(self) -> int:  # type: ignore
+    def getEffectiveLevel(self) -> int:  # type: ignore pylint: disable=invalid-name
         """
         Get the effective level for this logger.
 
@@ -83,7 +166,7 @@ class LoggerProtocol(Protocol):
         looking for a non-zero logging level. Return the first one found.
         """
 
-    def getChild(self, suffix: str) -> "Self":  # type: ignore
+    def getChild(self, suffix: str) -> "Self":  # type: ignore pylint: disable=invalid-name
         """
         Get a logger which is a descendant to this one.
 
@@ -102,8 +185,11 @@ class LoggerProtocol(Protocol):
 
     if sys.version_info >= (3, 12):
 
-        def getChildren(self) -> set["Logger"]: ...
+        def getChildren(self) -> Set["Logger"]:  # pylint: disable=invalid-name
+            ...
 
+    # Aligned with the typeshed type definition:
+    # `*kwargs` replaced with: `exc_info`, `stack_info`, `stacklevel`, `extra`
     def debug(
         self,
         msg: object,
@@ -122,6 +208,8 @@ class LoggerProtocol(Protocol):
         logger.debug("Houston, we have a %s", "thorny problem", exc_info=1)
         """
 
+    # Aligned with the typeshed type definition:
+    # `*kwargs` replaced with: `exc_info`, `stack_info`, `stacklevel`, `extra`
     def info(
         self,
         msg: object,
@@ -140,6 +228,18 @@ class LoggerProtocol(Protocol):
         logger.info("Houston, we have a %s", "interesting problem", exc_info=1)
         """
 
+    if sys.version_info >= (3, 12):
+        info.__doc__ = """
+        Log 'msg % args' with severity 'INFO'.
+
+        To pass exception information, use the keyword argument exc_info with
+        a true value, e.g.
+
+        logger.info("Houston, we have a %s", "notable problem", exc_info=1)
+        """
+
+    # Aligned with the typeshed type definition:
+    # `*kwargs` replaced with: `exc_info`, `stack_info`, `stacklevel`, `extra`
     def warning(
         self,
         msg: object,
@@ -158,7 +258,9 @@ class LoggerProtocol(Protocol):
         logger.warning("Houston, we have a %s", "bit of a problem", exc_info=1)
         """
 
-    def warn(
+    # Aligned with the typeshed type definition:
+    # `*kwargs` replaced with: `exc_info`, `stack_info`, `stacklevel`, `extra`
+    def warn(  # pylint: disable=invalid-name disable=missing-docstring
         self,
         msg: object,
         *args: object,
@@ -166,11 +268,10 @@ class LoggerProtocol(Protocol):
         stack_info: bool = False,
         stacklevel: int = 1,
         extra: Optional[Mapping[str, object]] = None,
-    ) -> None:
-        """
-        The 'warn' method is deprecated, use 'warning' instead.
-        """
+    ) -> None: ...
 
+    # Aligned with the typeshed type definition:
+    # `*kwargs` replaced with: `exc_info`, `stack_info`, `stacklevel`, `extra`
     def error(
         self,
         msg: object,
@@ -189,6 +290,8 @@ class LoggerProtocol(Protocol):
         logger.error("Houston, we have a %s", "major problem", exc_info=1)
         """
 
+    # Aligned with the typeshed type definition:
+    # `*kwargs` replaced with: `exc_info`, `stack_info`, `stacklevel`, `extra`
     def exception(
         self,
         msg: object,
@@ -202,6 +305,8 @@ class LoggerProtocol(Protocol):
         Convenience method for logging an ERROR with exception information.
         """
 
+    # Aligned with the typeshed type definition:
+    # `*kwargs` replaced with: `exc_info`, `stack_info`, `stacklevel`, `extra`
     def critical(
         self,
         msg: object,
@@ -220,6 +325,8 @@ class LoggerProtocol(Protocol):
         logger.critical("Houston, we have a %s", "major disaster", exc_info=1)
         """
 
+    # Aligned with the typeshed type definition:
+    # `*kwargs` replaced with: `exc_info`, `stack_info`, `stacklevel`, `extra`
     def log(
         self,
         level: int,
@@ -255,20 +362,34 @@ class LoggerProtocol(Protocol):
         """
 
     fatal = critical
+    if sys.version_info >= (3, 10):
 
-    def addHandler(self, hdlr: "Handler") -> None:
+        def fatal(
+            self,
+            msg: object,
+            *args: object,
+            exc_info: "_ExcInfoType" = None,
+            stack_info: bool = False,
+            stacklevel: int = 1,
+            extra: Optional[Mapping[str, object]] = None,
+        ) -> None:
+            """
+            Don't use this method, use critical() instead.
+            """
+
+    def addHandler(self, hdlr: "Handler") -> None:  # pylint: disable=invalid-name
         """
         Add the specified handler to this logger.
         """
 
-    def removeHandler(self, hdlr: "Handler") -> None:
+    def removeHandler(self, hdlr: "Handler") -> None:  # pylint: disable=invalid-name
         """
         Remove the specified handler from this logger.
         """
 
-    def findCaller(
+    def findCaller(  # pylint: disable=invalid-name
         self, stack_info: bool = False, stacklevel: int = 1
-    ) -> tuple[str, int, str, Optional[str]]:  # type: ignore
+    ) -> Tuple[str, int, str, Optional[str]]:  # type: ignore
         """
         Find the stack frame of the caller so that we can note the source
         file name, line number and function name.
@@ -282,7 +403,7 @@ class LoggerProtocol(Protocol):
         well as those created locally. Logger-level filtering is applied.
         """
 
-    def makeRecord(
+    def makeRecord(  # pylint: disable=invalid-name
         self,
         name: str,
         level: int,
@@ -300,7 +421,7 @@ class LoggerProtocol(Protocol):
         specialized LogRecords.
         """
 
-    def hasHandlers(self) -> bool:  # type: ignore
+    def hasHandlers(self) -> bool:  # type: ignore pylint: disable=invalid-name
         """
         See if this logger has any handlers configured.
 
@@ -311,7 +432,7 @@ class LoggerProtocol(Protocol):
         is checked for the existence of handlers.
         """
 
-    def callHandlers(self, record: "LogRecord") -> None:
+    def callHandlers(self, record: "LogRecord") -> None:  # pylint: disable=invalid-name
         """
         Pass a record to all relevant handlers.
 
@@ -329,7 +450,7 @@ class ALogLoggerProtocol(LoggerProtocol):
     stdlib `logging.Logger` instances.
     """
 
-    def isEnabled(self, level: "_Level") -> bool:  # type: ignore
+    def isEnabled(self, level: "_Level") -> bool:  # type: ignore pylint: disable=invalid-name
         """
         Is this logger enabled for level 'level'?
 
